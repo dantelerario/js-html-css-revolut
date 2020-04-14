60fdf4a27e9d58ac52968f155cf38b079768c6aa
Dante L'Erario <dantelerario@gmail.com>
2020-04-14 18:02:01 +0200

    non riesco a far funzionare lo script, dice che la var dropdown non è definita

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,19 @@
 $(document).ready( function() {
 
+  console.log('ok');
+
+  var dropDown = $('.menu .link.drop');
+  console.log(dropDown);
+  var dropLinks = dropDown.children('a');
+  var dropMenus = dropDrown.children('.drop-down');
+
+  dropLinks.click( function() {
+
+    var actualMenu = $(this).next('.drop-down');
+
+    actualMenu.toggle();
+
+  } );
+
 
 });
